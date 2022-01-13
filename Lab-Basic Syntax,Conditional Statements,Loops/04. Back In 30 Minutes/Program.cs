0e92d46664fa5f0c3440a9a29f4c0dc7a3bb331b
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-01-13 11:32:36 +0200

    new

---
+++ Lab-Basic Syntax,Conditional Statements,Loops/04. Back In 30 Minutes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _04._Back_In_30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            minutes += 30;
+
+            if (minutes >= 60)
+            {
+                hours++;
+                minutes -= 60;
+            }
+
+            if (hours > 23)
+            {
+                hours -= 24;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
