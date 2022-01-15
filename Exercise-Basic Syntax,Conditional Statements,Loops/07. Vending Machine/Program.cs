e5c287123d32564253dcc5a22fb2c85e00b2abbe
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-01-15 17:38:24 +0200

    New

--- Exercise-Basic Syntax,Conditional Statements,Loops/07. Vending Machine/Program.cs
+++ Exercise-Basic Syntax,Conditional Statements,Loops/07. Vending Machine/Program.cs
@@ -14,7 +14,7 @@ namespace _07._Vending_Machine
             {
                 double currentCoin = double.Parse(input);
 
-                if (currentCoin == 0.1 || currentCoin == 0.2 || currentCoin == 0.5 || currentCoin== 1 || currentCoin == 2)
+                if (currentCoin == 0.1 || currentCoin == 0.2 || currentCoin == 0.5 || currentCoin == 1 || currentCoin == 2)
                 {
                     money += currentCoin;
                 }
@@ -25,76 +25,61 @@ namespace _07._Vending_Machine
 
                 input = Console.ReadLine();
             }
-
             input = Console.ReadLine();
-
             while (input != "End")
             {
-                switch (input)
+                if (input == "Nuts" || input == "Water" || input == "Crisps" || input == "Soda" || input == "Coke")
                 {
-                    case "Nuts":
-                        if (money >= 2)
-                        {
-                            Console.WriteLine($"Purchased {input.ToLower()}");
-                            money -= 2;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sorry, not enough money");
-                        }
-                        break;
-                    case "Water":
-                        if (money >= 0.7)
-                        {
-                            Console.WriteLine($"Purchased {input.ToLower()}");
-                            money -= 0.7;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sorry, not enough money");
-                        }
-                        break;
-                    case "Crisps":
-                        if (money >= 1.5)
-                        {
-                            Console.WriteLine($"Purchased {input.ToLower()}");
-                            money -= 1.5;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sorry, not enough money");
-                        }
-                        break;
-                    case "Soda":
-                        if (money >= 0.8)
-                        {
-                            Console.WriteLine($"Purchased {input.ToLower()}");
-                            money -= 0.8;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sorry, not enough money");
-                        }
-                        break;
-                    case "Coke":
-                        if (money >= 1)
-                        {
-                            Console.WriteLine($"Purchased {input.ToLower()}");
-                            money -= 1;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sorry, not enough money");
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Invalid product");
-                        break;
+                    bool isBought = false;
+                    switch (input)
+                    {
+                        case "Nuts":
+                            if (money >= 2)
+                            {
+                                isBought = true;
+                                money -= 2;
+                            }
+                            break;
+                        case "Water":
+                            if (money >= 0.7)
+                            {
+                                isBought = true;
+                                money -= 0.7;
+                            }
+                            break;
+                        case "Crisps":
+                            if (money >= 1.5)
+                            {
+                                isBought = true;
+                                money -= 1.5;
+                            }
+                            break;
+                        case "Soda":
+                            if (money >= 0.8)
+                            {
+                                isBought = true;
+                                money -= 0.8;
+                            }
+                            break;
+                        case "Coke":
+                            if (money >= 1)
+                            {
+                                isBought = true;
+                                money -= 1;
+                            }
+                            break; 
+                    }
+                    Console.WriteLine(isBought ? $"Purchased {input.ToLower()}" : "Sorry, not enough money");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid product");
                 }
                 input = Console.ReadLine();
             }
             //ouput
             Console.WriteLine($"Change: {money:f2}");
-        }
+        } 
     }
+
 }
---
+++ Exercise-Basic Syntax,Conditional Statements,Loops/08. Triangle of Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08._Triangle_of_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= num; row++)
+            {
+                for (int colum = 1; colum <= row; colum++)
+                {
+                    Console.Write(row + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
