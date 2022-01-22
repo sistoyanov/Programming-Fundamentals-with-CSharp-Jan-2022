dea698195dec36363af8071113976cb9ccba1a2a
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-01-22 15:57:37 +0200

    Update

--- Arrays - Exercise/09. Kamino Factory/Program.cs
+++ Arrays - Exercise/09. Kamino Factory/Program.cs
@@ -9,47 +9,86 @@ namespace _09._Kamino_Factory
         {
             int num = int.Parse(Console.ReadLine());
             string input = Console.ReadLine();
-            
-            int[] bestDNS = new int[num];
-            int sampleCounter = 0;
-            int bestSampleNumber = 0;
 
-            int bestSampOnesCount = 0;
+            int sampleCounter = 0;
+
+            int[] bestDNA = new int[num];
+            int bestLenght = 1;
+            int bestStartIndex = 0;
+            int bestDNASum = 0;
+            int bestSampleCount = 0;
 
             while (input != "Clone them!")
             {
-                int currentOnesCounter = 0;
                 sampleCounter++;
-                int[] numbers = new int[num];
-                numbers = input.Split("!", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                
+                int[] currentDNA = new int[num];
+                currentDNA = input.Split("!", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
-                for (int j = 0; j < numbers.Length; j++)
+                int lenght = 1;
+                int bestCurrentLenght = 1;
+                int startIndex = 0;
+                int currentDNASum = 0;
+
+                for (int i = 0; i < currentDNA.Length - 1; i++)
                 {
-                    if (numbers[j] == 1)
+
+                    if (currentDNA[i] == currentDNA[i + 1])
                     {
-                        currentOnesCounter++;
+                        lenght++;
                     }
+                    else
+                    {
+                        lenght = 1;
+                    }
+
+                    if (lenght > bestCurrentLenght)
+                    {
+                        bestCurrentLenght = lenght;
+                        startIndex = i;
+                    }
+
+                    currentDNASum += currentDNA[i];
                 }
 
-                for (int i = 0; i < numbers.Length - 1; i++)
+                currentDNASum += currentDNA[num - 1];
+
+                if (bestCurrentLenght > bestLenght)
                 {
-
-                    if (numbers[i] == numbers[i + 1] && numbers[i] == 1 && numbers[i + 1] == 1)
+                    bestLenght = bestCurrentLenght;
+                    bestStartIndex = startIndex;
+                    bestDNASum = currentDNASum;
+                    bestSampleCount = sampleCounter;
+                    bestDNA = currentDNA;
+                }
+                else if (bestCurrentLenght == bestLenght)
+                {
+                    if (startIndex < bestStartIndex)
                     {
-                     
-                        bestDNS = numbers;
-                        bestSampleNumber = sampleCounter;
-                        bestSampOnesCount = currentOnesCounter;
-
+                        bestLenght = bestCurrentLenght;
+                        bestStartIndex = startIndex;
+                        bestDNASum = currentDNASum;
+                        bestSampleCount = sampleCounter;
+                        bestDNA = currentDNA;
                     }
-
+                    else if (startIndex == bestStartIndex)
+                    {
+                        if (currentDNASum > bestDNASum)
+                        {
+                            bestLenght = bestCurrentLenght;
+                            bestStartIndex = startIndex;
+                            bestDNASum = currentDNASum;
+                            bestSampleCount = sampleCounter;
+                            bestDNA = currentDNA;
+                        }
+                    }
                 }
 
                 input = Console.ReadLine();
             }
 
-            Console.WriteLine($"Best DNA sample {bestSampleNumber} with sum: {bestSampOnesCount}.");
-            Console.WriteLine(String.Join(" ", bestDNS));
+            Console.WriteLine($"Best DNA sample {bestSampleCount} with sum: {bestDNASum}.");
+            Console.WriteLine(String.Join(" ", bestDNA));
         }
     }
 }
---
+++ Arrays - Exercise/10. LadyBugs/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._LadyBugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int filedSize = int.Parse(Console.ReadLine());
+            int[] initialIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+
+            }
+
+        }
+    }
+}
