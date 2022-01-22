74575d069fd97fcbd25597dae3e8d14747a65ad4
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-01-22 17:32:27 +0200

    Update

--- Arrays - Exercise/10. LadyBugs/Program.cs
+++ Arrays - Exercise/10. LadyBugs/Program.cs
@@ -8,12 +8,41 @@ namespace _10._LadyBugs
         static void Main(string[] args)
         {
             int filedSize = int.Parse(Console.ReadLine());
+
+            int[] filed = new int[filedSize];
+
             int[] initialIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < initialIndexes.Length; i++)
+            {
+                for (int j = 0; j < filed.Length; j++)
+                {
+                    if (initialIndexes[i] == j)
+                    {
+                        filed[j] = 1;
+                    }
+                }
+            }
+
+            //Console.WriteLine(String.Join(" ", filed));
+
             string input = Console.ReadLine();
 
             while (input != "end")
             {
+                string[] currentComand = input.Split().ToArray();
+                int ladyBugIndex = int.Parse(currentComand[0]);
+                string ladyBugDirection = currentComand[1].ToString();
+                int ladyBugFlyLength = int.Parse(currentComand[2]);
 
+
+                //Console.WriteLine(ladyBugIndex);
+                //Console.WriteLine(ladyBugDirection);
+                //Console.WriteLine(ladyBugFlyLength);
+
+
+
+                input = Console.ReadLine();
             }
 
         }
