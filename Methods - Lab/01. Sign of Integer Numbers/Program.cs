0b18710a805e0ebd8edf14453ea5ccbc7d658e7a
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-01-26 08:32:36 +0200

    Update

---
+++ Methods - Lab/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
