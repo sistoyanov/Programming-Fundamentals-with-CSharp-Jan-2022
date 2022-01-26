57cb7bd7016ba3395d6180e278d55c6655a06a78
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-01-26 08:29:36 +0200

    Update

---
+++ Arrays - More Exercise/01. Encrypt, Sort and Print Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _01._Encrypt__Sort_and_Print_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            int[] array = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                string currentName = Console.ReadLine();
+                int currentNameSum = 0;
+
+                for (int j = 0; j < currentName.Length; j++)
+                {
+
+                    if (currentName[j] == 'a' || currentName[j] == 'e' || currentName[j] == 'i' || currentName[j] == 'o' || currentName[j] == 'u' ||
+                        currentName[j] == 'A' || currentName[j] == 'E' || currentName[j] == 'I' || currentName[j] == 'O' || currentName[j] == 'U')
+                    {
+                        currentNameSum += (int)currentName[j] * currentName.Length;
+
+                    }
+                    else
+                    {
+                        currentNameSum += (int)currentName[j] / currentName.Length;
+                    }
+
+                }
+
+                array[i] = currentNameSum;
+            }
+
+            Array.Sort(array);
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
