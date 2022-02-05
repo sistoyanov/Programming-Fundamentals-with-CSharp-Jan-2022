199641a5d64599c0ea191a2bf10a0f4bec2c2718
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-02-05 15:22:23 +0200

    Upload

--- Methods - Exercise/11. Array Manipulator/Program.cs
+++ Methods - Exercise/11. Array Manipulator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace _11._Array_Manipulator
 {
@@ -15,44 +16,28 @@ namespace _11._Array_Manipulator
             while ((input = Console.ReadLine()) != "end")
             {
                 string[] currentComand = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
-
-                //Console.WriteLine(String.Join("", currentComand));
-                //Console.WriteLine(String.Join("", array));
                 
                 if (currentComand[0] == "exchange")
                 {
-                    long splitIndex = long.Parse(currentComand[1]);
-
-                    if (splitIndex == array.Length - 1)
-                    {
-                        continue;
-                    }
+                    int splitIndex = int.Parse(currentComand[1]);
 
                     if (splitIndex >= 0 && splitIndex < array.Length)
                     {
                         int[] splitedArr = new int[array.Length];
 
-                        //if (splitIndex == 0)
-                        //{
-                        //    splitedArr[splitedArr.Length - 1] = array[0];
-                        //}
-
-                        var firstSplitArray = splitIndex + 1;
-                        var secondSplitArray = array.Length - firstSplitArray;
-
-                        for (long i = splitIndex + 1; i < splitedArr.Length; i++)
+                        for (int i = 0; i < array.Length; i++)
                         {
-                            splitedArr[i - (splitIndex + 1)] = array[i];
-                        }
-
-                        for (long j = 0; j < firstSplitArray; j++)
-                        {
-                            splitedArr[] = array[j];
-
+                            if (i < array.Length - (splitIndex + 1))
+                            {
+                                splitedArr[i] = array[splitIndex + 1 + i];
+                            }
+                            else
+                            {
+                                splitedArr[i] = array[i - (array.Length - (splitIndex + 1))];
+                            }
                         }
 
                         array = splitedArr;
-
 
                     }
                     else
@@ -62,17 +47,230 @@ namespace _11._Array_Manipulator
                 }
                 else if (currentComand[0] == "max" || currentComand[0] == "min")
                 {
+                    int ouput = 0;
+                    bool isMaxEvenOrOddFound = false;
+                    int maxEven = int.MinValue;
+                    int maxOdd = int.MinValue;
+                    int minEven = int.MaxValue;
+                    int minOdd = int.MaxValue;
 
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        
+                        
+                        if (currentComand[0] == "max" && currentComand[1] == "even" && array[i] % 2 == 0)
+                        {
+                            
+                            if (array[i] > maxEven)
+                            {
+                                maxEven = array[i];
+                                ouput = i;
+                            }
+                            
+                            isMaxEvenOrOddFound = true;
+                        }
+                        else if (currentComand[0] == "max" && currentComand[1] == "odd" && array[i] % 2 != 0)
+                        {
+                            
+                            if (array[i] > maxOdd)
+                            {
+                                maxOdd = array[i];
+                                ouput = i;
+                            }
+
+                            isMaxEvenOrOddFound = true;
+                        }
+                        else if (currentComand[0] == "min" && currentComand[1] == "even" && array[i] % 2 == 0)
+                        {
+
+                            if (array[i] < minEven)
+                            {
+                                minEven = array[i];
+                                ouput = i;
+                            }
+
+                            isMaxEvenOrOddFound = true;
+                        }
+                        else if (currentComand[0] == "min" && currentComand[1] == "odd" && array[i] % 2 != 0)
+                        {
+                            
+                            if (array[i] < minOdd)
+                            {
+                                minOdd = array[i];
+                                ouput = i;
+                            }
+
+                            isMaxEvenOrOddFound = true;
+                        }
+                    }
+
+                    Console.WriteLine(isMaxEvenOrOddFound ? $"{ouput}" : "No matches");
                 }
                 else if (currentComand[0] == "first" || currentComand[0] == "last")
                 {
+                    int counter = int.Parse(currentComand[1]);
+                    StringBuilder evens = new StringBuilder();
+                    StringBuilder odds = new StringBuilder();
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        
+                        if (array[i] % 2 == 0)
+                        {
+                            evens.Append(array[i]);
+                        }
+                        else
+                        {
+                            odds.Append(array[i]);
+                        }
+                    }
+
+                    bool isCounterGreater = true;
+                    StringBuilder output = new StringBuilder();
+
+                    if (counter <= array.Length)
+                    {
+                        isCounterGreater = false;
+                        int[] evensArray = new int[evens.Length];
+                        int[] oddsArray = new int[odds.Length];
+
+                        for (int h = 0; h < evensArray.Length; h++)
+                        {
+                            evensArray[h] = int.Parse(evens[h].ToString());
+                        }
+
+                        for (int k = 0; k < oddsArray.Length; k++)
+                        {
+                            oddsArray[k] = int.Parse(odds[k].ToString());
+                        }
+
+
+                        if (currentComand[0] == "first")
+                        {
+                            if (currentComand[2] == "even" && evens.Length > 0)
+                            {
+                           
+                                if (counter >= evensArray.Length)
+                                {
+                                    for (int i = 0; i < evensArray.Length; i++)
+                                    {
+                                        output.Append(int.Parse(evensArray[i].ToString()));
+                                    }
+                                }
+                                else
+                                {
+                                    for (int j = 0; j < counter; j++)
+                                    {
+                                        output.Append(int.Parse(evensArray[j].ToString()));
+                                    }
+                                }
+
+                            }
+                            else if (currentComand[2] == "odd" && odds.Length > 0)
+                            {
+
+                                if (counter >= oddsArray.Length)
+                                {
+                                    for (int i = 0; i < oddsArray.Length; i++)
+                                    {
+                                        output.Append(int.Parse(oddsArray[i].ToString()));
+                                    }
+                                }
+                                else
+                                {
+                                    for (int j = 0; j < counter; j++)
+                                    {
+                                        output.Append(int.Parse(oddsArray[j].ToString()));
+                                    }
+                                }
+                            }
+                        }
+                        else if (currentComand[0] == "last")
+                        {
+                            if (currentComand[2] == "even" && evens.Length > 0)
+                            {
+
+                                if (counter == evensArray.Length)
+                                {
+                                    for (int i = 0 ; i < evensArray.Length; i++)
+                                    {
+                                        output.Append(int.Parse(evensArray[(evensArray.Length - counter) + i].ToString()));
+                                    }
+                                }
+                                else if (counter > evensArray.Length)
+                                {
+                                    for (int i = 0; i < evensArray.Length; i++)
+                                    {
+                                        output.Append(int.Parse(evensArray[i].ToString()));
+                                    }
+                                }
+                                else
+                                {
+                                    for (int j = 0; j < counter; j++)
+                                    {
+                                        output.Append(int.Parse(evensArray[(evensArray.Length - counter) + j].ToString()));
+                                    }
+                                }
+
+
+                            }
+                            else if (currentComand[2] == "odd" && odds.Length > 0)
+                            {
+                                if (counter == oddsArray.Length)
+                                {
+                                    for (int i = 0; i < oddsArray.Length; i++)
+                                    {
+                                        output.Append(int.Parse(oddsArray[(oddsArray.Length - counter) + i].ToString()));
+                                    }
+                                }
+                                else if (counter > oddsArray.Length)
+                                {
+                                    for (int i = 0; i < oddsArray.Length; i++)
+                                    {
+                                        output.Append(int.Parse(oddsArray[i].ToString()));
+                                    }
+                                }
+                                else
+                                {
+                                    for (int j = 0; j < counter; j++)
+                                    {
+                                        output.Append(int.Parse(oddsArray[(oddsArray.Length - counter) + j].ToString()));
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+
+                    if (isCounterGreater)
+                    {
+                        Console.WriteLine("Invalid count");
+                    }
+                    else
+                    {
+                        if (output.Length > 0)
+                        {
+                            string[] consoleOuput = new string[output.Length];
+
+                            for (int i = 0; i < output.Length; i++)
+                            {
+                                consoleOuput[i] = output[i].ToString();
+                            }
+
+                            Console.WriteLine($"[{string.Join(", ", consoleOuput)}]");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"[]");
+                        }
+                    }
 
                 }
 
-                Console.WriteLine($"[{String.Join(" " + ",", array)}]");
+              
             }
 
-            Console.WriteLine($"[{String.Join(" " + ",", array)}]");
+            Console.WriteLine($"[{String.Join("," + " ", array)}]");
         }
     }
 }
--- Methods - Exercise/Test/Program.cs
+++ Methods - Exercise/Test/Program.cs
@@ -7,40 +7,254 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            int[] array = input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
-                    .Select(int.Parse)
-                    .ToArray();
-
-            string[] currentComand = input.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
-
-            long splitIndex = long.Parse(currentComand[1]);
-
-            int[] left = new int[splitIndex + 1];
-            int[] right = new int[array.length - left.length];
-
-            for (int i = 0; i < left.length; i++)
-            {
-                left[i] = array[i];
-            }
-
-            int nextIndex = 0;
+           int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+           string commmand;
             
-            for (long i = splitIndex + 1; i < array.length; i++)
+            while ((commmand = Console.ReadLine()) != "end")
             {
-                right[nextIndex] = array[i];
-                nextIndex++;
+                string[] cmdArg = commmand.Split(' ');
+
+                string cmdType = cmdArg[0];
+
+                if (cmdType == "exchange")
+                {
+                    int exchangeIndex = int.Parse(cmdArg[1]);
+
+                    if (exchangeIndex < 0 || exchangeIndex >= numbers.Length)
+                    {
+                        Console.WriteLine("Invalid index");
+                        continue;
+                    }
+
+                    numbers = ExchangeArrayParts(numbers, exchangeIndex);
+                }
+                
+                else if (cmdType == "max" || cmdType == "min")
+                {
+                    string oddOrEven = cmdArg[1];
+
+                    int maxIndex = MaxElementOfTypeIndex(numbers, oddOrEven);
+                    int minIndex = MinElementOfTypeIndex(numbers, oddOrEven);
+
+                    if (maxIndex == -1 || minIndex == -1)
+                    {
+                        Console.WriteLine("No matches");
+                    }
+                    else
+                    {
+                        if (cmdType == "max")
+                        {
+                            Console.WriteLine(maxIndex);
+                        }
+                        else
+                        {
+                            Console.WriteLine(minIndex);
+                        }
+                        
+                    }
+                }
+
+                else if (cmdType == "first" || cmdType == "last")
+                {
+                    int count = int.Parse(cmdArg[1]);
+                    string oddOrEven =cmdArg[2];
+
+                    if (count > numbers.Length)
+                    {
+                        Console.WriteLine("Invalid count");
+                        continue;
+                    }
+
+                    if (cmdType == "first")
+                    {
+                        PrintsFirstElementsOfType(numbers, count, oddOrEven);
+                    }
+                    else
+                    {
+                        PrintsLastElementsOfType(numbers, count, oddOrEven);
+                    }
+
+                }
             }
 
-            for (int i = 0; i < right.length; i++)
+            Console.WriteLine(ArrayToStringFormat(numbers, numbers.Length));
+        }
+
+        static int[] ExchangeArrayParts(int[] numbers, int index)
+        {
+            int[] exchangedNumbers = new int[numbers.Length];
+            int exchangedNumbersIndex = 0;
+
+            for (int i = index + 1; i < numbers.Length; i++)
             {
-                array[i] = right[i];
+                exchangedNumbers[exchangedNumbersIndex] = numbers[i];
+                exchangedNumbersIndex++;
             }
 
-            for (int i = right.length; i < array.length; i++)
+            for (int i = 0; i <= index; i++)
             {
-                array[i] = left[i - right.length];
+                exchangedNumbers[exchangedNumbersIndex] = numbers[i];
+                exchangedNumbersIndex++;
             }
+
+            return exchangedNumbers;
+        }
+
+        static int MaxElementOfTypeIndex(int[] numbers, string oddOrEevn)
+        {
+            int index = -1;
+            int maxValue = int.MinValue;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNum = numbers[i];
+                
+                if (oddOrEevn == "even")
+                {
+                    if (currentNum % 2 == 0 && currentNum >= maxValue)
+                    {
+                        maxValue = currentNum;
+                        index = i;
+                    }
+                }
+                else if (oddOrEevn == "odd")
+                {
+                    if (currentNum % 2 != 0 && currentNum >= maxValue)
+                    {
+                        maxValue = currentNum;
+                        index = i;
+                    }
+                }
+            }
+
+            return index;
+        }
+
+        static int MinElementOfTypeIndex(int[] numbers, string oddOrEevn)
+        {
+            int index = -1;
+            int minValue = int.MaxValue;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNum = numbers[i];
+
+                if (oddOrEevn == "even")
+                {
+                    if (currentNum % 2 == 0 && currentNum <= minValue)
+                    {
+                        minValue = currentNum;
+                        index = i;
+                    }
+                }
+                else if (oddOrEevn == "odd")
+                {
+                    if (currentNum % 2 != 0 && currentNum <= minValue)
+                    {
+                        minValue = currentNum;
+                        index = i;
+                    }
+                }
+            }
+
+            return index;
+        }
+
+        static void PrintsFirstElementsOfType(int[] numbers, int count, string oddOrEven)
+        {
+            int[] firstElements = new int[count];
+            int firstElementsIndex = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currNum = numbers[i];
+
+                if (oddOrEven == "even")
+                {
+                    if (currNum % 2 == 0 && firstElementsIndex < count)
+                    {
+                        
+                        firstElements[firstElementsIndex] = currNum;
+                        firstElementsIndex++;
+                    }
+                }
+                else if (oddOrEven == "odd")
+                {
+                    if (currNum % 2 != 0 && firstElementsIndex < count)
+                    {
+                        
+                        firstElements[firstElementsIndex] = currNum;
+                        firstElementsIndex++;
+                    }
+                }
+            }
+
+            Console.WriteLine(ArrayToStringFormat(firstElements, firstElementsIndex));
+        }
+
+        static void PrintsLastElementsOfType(int[] numbers, int count, string oddOrEven)
+        {
+            int[] lastElements = new int[count];
+            int lastElementsIndex = 0;
+
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                int currNum = numbers[i];
+
+                if (oddOrEven == "even")
+                {
+                    if (currNum % 2 == 0 && lastElementsIndex < count)
+                    {
+
+                        lastElements[lastElementsIndex] = currNum;
+                        lastElementsIndex++;
+                    }
+                }
+                else if (oddOrEven == "odd")
+                {
+                    if (currNum % 2 != 0 && lastElementsIndex < count)
+                    {
+
+                        lastElements[lastElementsIndex] = currNum;
+                        lastElementsIndex++;
+                    }
+                }
+            }
+
+            int[] reverseArray = new int[lastElementsIndex];
+            int reverseArrayIndex = 0;
+
+            for (int i = lastElementsIndex - 1; i >= 0; i--)
+            {
+                reverseArray[reverseArrayIndex] = lastElements[i];
+                reverseArrayIndex++;
+            }
+
+            Console.WriteLine(ArrayToStringFormat(reverseArray, reverseArrayIndex));
+        }
+
+        static string ArrayToStringFormat(int[] arr, int elementsCount)
+        {
+
+            string output = string.Empty;
+            output += "[";
+
+            for (int i = 0; i < elementsCount; i++)
+            {
+                if (i == elementsCount - 1)
+                {
+                    output += $"{arr[i]}";
+                }
+                else
+                {
+                    output += $"{arr[i]}, ";
+                }
+            }
+
+            output += "]";
+
+            return output;
         }
     }
+  
 }
