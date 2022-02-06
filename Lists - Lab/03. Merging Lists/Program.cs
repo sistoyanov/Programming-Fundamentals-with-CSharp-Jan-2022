033a7d4709d44e85b0b89d3cc8e1e2b0d2dbd250
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-02-06 17:47:26 +0200

    update

---
+++ Lists - Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers1 = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> numbers2 = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            int maxCount = Math.Max(numbers1.Count, numbers2.Count);
+
+            for (int i = 0; i < maxCount; i++)
+            {
+                if (i < numbers1.Count)
+                {
+                    result.Add(numbers1[i]);
+                }
+
+                if (i < numbers2.Count)
+                {
+                    result.Add(numbers2[i]);
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", result));
+        }
+
+    }
+}
