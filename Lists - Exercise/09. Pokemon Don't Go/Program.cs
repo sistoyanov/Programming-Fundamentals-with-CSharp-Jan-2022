8556c9a66012e76c3f4f2faf8039f7b54f20f74e
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-02-13 12:39:14 +0200

    Upload

---
+++ Lists - Exercise/09. Pokemon Don't Go/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Pokemon_Don_t_Go
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int sum = 0;
+
+            while(numbers.Count > 0)
+            {
+                int currentIndex = int.Parse(Console.ReadLine());
+                int currentNum = 0;
+
+                if (currentIndex >= 0 && currentIndex < numbers.Count)
+                {
+                    currentNum = numbers[currentIndex];
+                    sum += currentNum;
+                    numbers.RemoveAt(currentIndex);
+                    IncreaseAndDecreaseNumbers(numbers, currentNum);
+                }
+                else if (currentIndex < 0)
+                {
+                    currentNum = numbers[0];
+                    sum += currentNum;
+                    numbers.RemoveAt(0);
+                    numbers.Insert(0, numbers[numbers.Count - 1]);
+                    IncreaseAndDecreaseNumbers(numbers, currentNum);
+                }
+                else if (currentIndex > numbers.Count - 1)
+                {
+                    currentNum = numbers[numbers.Count - 1];
+                    sum += currentNum;
+                    numbers.RemoveAt(numbers.Count - 1);
+                    numbers.Add(numbers[0]);
+                    IncreaseAndDecreaseNumbers(numbers, currentNum);
+                }
+
+            }
+
+            Console.WriteLine(sum);
+        }
+
+       static void IncreaseAndDecreaseNumbers(List<int> numbers, int currentNum)
+        {
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] <= currentNum)
+                {
+                    numbers[i] += currentNum;
+                }
+                else
+                {
+                    numbers[i] -= currentNum;
+                }
+            }
+        }
+    }
+}
---
+++ Lists - Exercise/10. SoftUni Course Planning/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10._SoftUni_Course_Planning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lessons = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "course start")
+            {
+                string[] commandArg = command.Split(":", StringSplitOptions.RemoveEmptyEntries);
+                string currentCommand = commandArg[0];
+                string lessonTitle = commandArg[1];
+
+                if (currentCommand == "Add")
+                {
+                    
+                    if (!lessons.Contains(lessonTitle))
+                    {
+                        lessons.Add(lessonTitle);
+                    }
+
+                }
+                else if (currentCommand == "Insert")
+                {
+                    int index = int.Parse(commandArg[2]);
+
+                    if (!lessons.Contains(lessonTitle))
+                    {
+                        lessons.Insert(index, lessonTitle);
+                    }
+
+                }
+                else if (currentCommand == "Remove")
+                {
+                    string exerciseTitle = $"{lessonTitle}-Exercise";
+                    
+                    if (lessons.Contains(lessonTitle))
+                    {
+                        lessons.Remove(lessonTitle);
+                    }
+
+                    if (lessons.Contains(exerciseTitle))
+                    {
+                        lessons.Remove(exerciseTitle);
+                    }
+                }
+                else if (currentCommand == "Swap")
+                {
+                    string lessonsTitleSecond = commandArg[2];
+                    string exerciseTitle = $"{lessonTitle}-Exercise";
+                    string exerciseTitleSecond = $"{lessonsTitleSecond}-Exercise";
+
+                    if (lessons.Contains(lessonTitle) && lessons.Contains(lessonsTitleSecond))
+                    {
+                        int lessonsTitleIndex = lessons.IndexOf(lessonTitle);
+                        int lessonsTitleSecondIndex = lessons.IndexOf(lessonsTitleSecond);
+
+                        lessons.RemoveAt(lessonsTitleIndex);
+                        lessons.Insert(lessonsTitleIndex, lessonsTitleSecond);
+
+                        lessons.RemoveAt(lessonsTitleSecondIndex);
+                        lessons.Insert(lessonsTitleSecondIndex, lessonTitle);
+
+                        if (lessons.Contains(exerciseTitleSecond))
+                        {
+                            int exerciseTitleSecondIndex = lessons.IndexOf(exerciseTitleSecond);
+                            lessons.RemoveAt(exerciseTitleSecondIndex);
+                            lessons.Insert(lessonsTitleIndex + 1, exerciseTitleSecond);
+                        }
+
+                        if (lessons.Contains(exerciseTitle))
+                        {
+                            int exerciseTitleIndex = lessons.IndexOf(exerciseTitle);
+                            lessons.RemoveAt(exerciseTitleIndex);
+                            lessons.Insert(lessonsTitleSecondIndex + 1, exerciseTitle);
+                        }
+
+                    }
+                }
+                else if (currentCommand == "Exercise")
+                {
+                    string exerciseTitle = $"{lessonTitle}-Exercise";
+
+                    if (lessons.Contains(lessonTitle))
+                    {
+                        int lessonTitleIndex = lessons.IndexOf(lessonTitle);
+
+                        if (!lessons.Contains(exerciseTitle))
+                        {
+                            lessons.Insert(lessonTitleIndex + 1, exerciseTitle);
+                        }
+       
+                    }
+                    else
+                    {
+                        lessons.Add(lessonTitle);
+                        lessons.Add(exerciseTitle);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            for (int i = 0; i < lessons.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{lessons[i]}");
+            }
+        }
+    }
+}
