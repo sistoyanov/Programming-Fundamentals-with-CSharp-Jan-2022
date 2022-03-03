56d6ce082e6ec9ae4c75bb3eec2e34f888c98613
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-03-03 16:46:32 +0200

    Update

--- Associative Arrays - Exercise/01. Count Chars in a String/Program.cs
+++ Associative Arrays - Exercise/01. Count Chars in a String/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Count_Chars_in_a_String
 {
@@ -6,7 +8,34 @@ namespace _01._Count_Chars_in_a_String
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<char, int> chars = new Dictionary<char, int>();
+
+            foreach (var word in words)
+            {
+                char[] currentCahrs = word.ToCharArray();
+
+                foreach (char item in currentCahrs)
+                {
+
+                    if (chars.ContainsKey(item))
+                    {
+                        chars[item] += 1;
+                    }
+                    else
+                    {
+                        chars.Add(item, 1);
+                    }
+
+                }
+            }
+
+            foreach (var item in chars)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
         }
     }
 }
---
+++ Associative Arrays - Exercise/02. A Miner Task/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            string input = string.Empty;
+            Dictionary<string, int> resourses = new Dictionary<string, int>();
+
+            string currentResource = string.Empty;
+            int currentQuantity = 0;
+
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                counter++;
+                
+
+                if (counter % 2 != 0)
+                {
+                    currentResource = input;
+
+                    if (!resourses.ContainsKey(currentResource))
+                    {
+                        resourses.Add(currentResource, currentQuantity);
+                    }
+                    else
+                    {
+                        resourses[currentResource] += currentQuantity;
+                        currentQuantity = 0;
+                    }
+                   
+                }
+                else
+                {
+                    currentQuantity = int.Parse(input);
+                    resourses[currentResource] += currentQuantity;
+                    currentQuantity = 0;
+                }
+            }
+
+            foreach (var resource in resourses)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+        }
+    }
+}
