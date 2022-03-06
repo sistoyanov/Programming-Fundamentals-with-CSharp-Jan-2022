5203c283396e79472bf814baa9858b7ed2ab9ebc
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-03-06 18:26:52 +0200

    Update

---
+++ Associative Arrays - Exercise/03. Legendary Farming/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Legendary_Farming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> keyMaterials = new Dictionary<string, int>()
+            {
+                {"shards", 0},
+                {"motes", 0},
+                {"fragments", 0},
+            };
+
+            Dictionary<string, int> junkMaterials = new Dictionary<string, int>();
+            bool isMaterilaCrafted = false;
+
+            while (!isMaterilaCrafted)
+            {
+                string[] inputArg = Console.ReadLine()
+                    .ToLower()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                for (int i = 0; i < inputArg.Length; i += 2)
+                {
+                    int currentQty = int.Parse(inputArg[i]);
+                    string currentMaterial = inputArg[i + 1];
+
+                    if (keyMaterials.ContainsKey(currentMaterial))
+                    {
+                        string craftedMaterial = AddKeyMaterial(keyMaterials, currentQty, currentMaterial);
+
+                        if (!String.IsNullOrEmpty(craftedMaterial))
+                        {
+                            Console.WriteLine($"{craftedMaterial} obtained!");
+                            isMaterilaCrafted = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        AddJunkMaterial(junkMaterials, currentQty, currentMaterial);
+                    }
+                }
+            }
+
+            PrintMaterials(keyMaterials);
+            PrintMaterials(junkMaterials);
+
+            static string AddKeyMaterial(Dictionary<string, int> keyMaterials, int currentQty, string currentMaterial)
+            {
+                string output = string.Empty;
+                const int CRAFTVALUE = 250;
+                
+                if (currentMaterial == "shards")
+                { 
+                    keyMaterials[currentMaterial] += currentQty;
+
+                    if (keyMaterials[currentMaterial] >= CRAFTVALUE)
+                    {
+                        output = "Shadowmourne";
+                        keyMaterials[currentMaterial] -= 250;
+                    }
+                }
+                else if (currentMaterial == "fragments")
+                {
+                    keyMaterials[currentMaterial] += currentQty;
+
+                    if (keyMaterials[currentMaterial] >= CRAFTVALUE)
+                    {
+                        output = "Valanyr";
+                        keyMaterials[currentMaterial] -= 250;
+                    }
+                }
+                else if (currentMaterial == "motes")
+                {
+                    keyMaterials[currentMaterial] += currentQty;
+
+                    if (keyMaterials[currentMaterial] >= CRAFTVALUE)
+                    {
+                        output = "Dragonwrath";
+                        keyMaterials[currentMaterial] -= 250;
+                    }
+                }
+
+                return output;
+            }
+
+            static void AddJunkMaterial(Dictionary<string, int> junkMaterials, int currentQty, string currentMaterial)
+            {
+                if (!junkMaterials.ContainsKey(currentMaterial))
+                {
+                    junkMaterials.Add(currentMaterial, 0);
+                }
+
+                junkMaterials[currentMaterial] += currentQty;
+            }
+
+            static void PrintMaterials(Dictionary<string, int> materialsCollection)
+            {
+                foreach (var kvp in materialsCollection)
+                {
+                    Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Associative Arrays - Exercise/04. Orders/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Orders
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "buy")
+            {
+                string[] inputArg = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string currentProductName = inputArg[0];
+                double currentProductPrice = double.Parse(inputArg[1]);
+                double currentProductQty = double.Parse(inputArg[2]);
+
+                if(!products.Any(p => p.Name == currentProductName))
+                {
+                    products.Add(new Product (currentProductName, currentProductPrice, currentProductQty));
+                }
+                else
+                {
+                    Product searchedProduct = products.Find(p => p.Name == currentProductName);
+
+                    searchedProduct.Qty += currentProductQty;
+
+                    if (searchedProduct.Price != currentProductPrice)
+                    {
+                        searchedProduct.Price = currentProductPrice;
+                    }
+
+                }
+                
+            }
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"{product.Name} -> {(product.Price * product.Qty):f2}");
+            }
+        }
+    }
+
+    class Product
+    {
+        public Product(string name, double price, double qty)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Qty = qty;
+        }
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Qty { get; set; }
+    }
+}
