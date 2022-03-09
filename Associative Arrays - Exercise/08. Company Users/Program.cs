4416b05872c83b610925f2523ba831cb2ecb557c
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-03-09 09:36:11 +0200

    Upload

---
+++ Associative Arrays - Exercise/08. Company Users/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] inputArg = input.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string companyName = inputArg[0];
+                string employeeId = inputArg[1];
+                
+                if (!companies.ContainsKey(companyName))
+                {
+                    companies.Add(companyName, new List<string>());
+                }
+
+                List<string> currentCompanyList = companies[companyName];
+
+                if (!currentCompanyList.Contains(employeeId))
+                {
+                    currentCompanyList.Add(employeeId);
+                }
+
+            }
+
+            foreach (var kvp in companies)
+            {
+                Console.WriteLine($"{kvp.Key}");
+
+                foreach (string employeeId in kvp.Value)
+                {
+                    Console.WriteLine($"-- {employeeId}");
+                }
+            }
+        }
+    }
+}
