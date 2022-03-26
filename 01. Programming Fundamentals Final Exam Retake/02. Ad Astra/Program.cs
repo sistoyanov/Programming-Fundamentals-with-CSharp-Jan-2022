8ecbb97f63200dc8ace3bf872a68e10ca679c910
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-03-26 14:06:12 +0200

    Upload

--- 01. Programming Fundamentals Final Exam Retake/02. Ad Astra/Program.cs
+++ 01. Programming Fundamentals Final Exam Retake/02. Ad Astra/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace _02._Ad_Astra
 {
@@ -6,7 +7,22 @@ namespace _02._Ad_Astra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            int totalCalories = 0;
+            Regex regex = new Regex(@"(\||\#)(?<item>[A-Za-z]+\s?[A-Za-z]*)(\1)(?<date>[\d]{2}\/[\d]{2}\/[\d]{2})(\1)(?<calories>[\d]+)(\1)");
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                totalCalories += int.Parse(match.Groups["calories"].Value);
+            }
+            
+            Console.WriteLine($"You have food to last you for: {totalCalories / 2000} days!");
+
+            foreach (Match currentMatch in matches)
+            {
+                Console.WriteLine($"Item: {currentMatch.Groups["item"].Value}, Best before: {currentMatch.Groups["date"].Value}, Nutrition: {currentMatch.Groups["calories"].Value}");
+            }
         }
     }
 }
