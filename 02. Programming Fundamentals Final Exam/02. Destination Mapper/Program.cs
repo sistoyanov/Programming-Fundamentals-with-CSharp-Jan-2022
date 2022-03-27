4152a25c533ddd20525e99e3c748e48c23a34b7e
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-03-27 12:45:43 +0200

    Upload

---
+++ 02. Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
