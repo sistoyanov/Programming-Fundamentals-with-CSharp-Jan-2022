003b68b08541fe1606b4d91d2ea6c84e6dc22d56
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-03-27 14:01:16 +0200

    Update

--- 02. Programming Fundamentals Final Exam/03. Plant Discovery/Program.cs
+++ 02. Programming Fundamentals Final Exam/03. Plant Discovery/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03._Plant_Discovery
 {
@@ -6,7 +8,106 @@ namespace _03._Plant_Discovery
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int count = int.Parse(Console.ReadLine());
+            List<Plant> plants = new List<Plant>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] plantDetails = Console.ReadLine().Split("<->", StringSplitOptions.RemoveEmptyEntries);
+                string plantName = plantDetails[0];
+                int plantRarity = int.Parse(plantDetails[1]);
+
+                if (!IsPlantExists(plants, plantName))
+                {
+                    Plant newPlant = new Plant(plantName, plantRarity);
+                    plants.Add(newPlant);
+                }
+                else
+                {
+                    Plant currentPlant = FindPlant(plants, plantName);
+                    currentPlant.Rarity = plantRarity;
+                }
+            }
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Exhibition") 
+            {
+                string[] commandArg = command
+                    .Split(new char[] { ':', '-' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(s => s.Trim())
+                    .ToArray();
+
+                string commandType = commandArg[0];
+                string currentPlantName = commandArg[1];
+
+                if (!IsPlantExists(plants, currentPlantName))
+                {
+                    Console.WriteLine("error");
+                    continue;
+                }
+
+                if (commandType == "Rate")
+                {
+
+                    double ratingToAdd = double.Parse(commandArg[2]);
+                    Plant plantToRate = FindPlant(plants, currentPlantName);
+                    plantToRate.Rating.Add(ratingToAdd);
+                }
+                else if (commandType == "Update")
+                {
+
+                    int rarityToUpdate = int.Parse(commandArg[2]);
+                    Plant plantToUpdate = FindPlant(plants, currentPlantName);
+                    plantToUpdate.Rarity = rarityToUpdate;
+                }
+                else if (commandType == "Reset")
+                {
+                    
+                    Plant plantToReset = FindPlant(plants, currentPlantName);
+                    plantToReset.Rating.Clear();
+                }
+            }
+
+            Console.WriteLine("Plants for the exhibition:");
+
+            foreach (Plant plant in plants)
+            {
+                double avarageRating = 0;
+                
+                if (plant.Rating.Count > 0)
+                {
+                    avarageRating = plant.Rating.Average();
+                }
+
+                Console.WriteLine($" - {plant.Name}; Rarity: {plant.Rarity}; Rating: {avarageRating:f2}");
+            }
         }
+
+        static bool IsPlantExists (List<Plant> plants, string plantName)
+        {
+            return plants.Any(n => n.Name == plantName);
+        }
+
+        static Plant FindPlant (List<Plant> plants, string plantName)
+        {
+            Plant searchedPlant = plants.FirstOrDefault(n => n.Name == plantName);
+            return searchedPlant;
+        }
+    }
+
+    class Plant
+    {
+        public Plant(string plantName, int plantRarity)
+        {
+            this.Name = plantName;
+            this.Rarity = plantRarity;
+            this.Rating = new List<double>();
+        }
+
+        public string Name { get; set; }
+        public double Rarity { get; set; }
+        public List<double> Rating { get; set; }
+
     }
 }
