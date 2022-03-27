fb8ccc76ede7dce29b41cd1efa347d22ad54077b
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-03-27 13:00:22 +0200

    Upload

--- 02. Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
+++ 02. Programming Fundamentals Final Exam/02. Destination Mapper/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace _02._Destination_Mapper
 {
@@ -6,7 +8,21 @@ namespace _02._Destination_Mapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+            int totalPoints = 0;
+            List<string> destinations = new List<string>();
+
+            Regex regex = new Regex(@"(\=|\/)(?<destination>[A-Z][A-Za-z]{2,})(\1)");
+            MatchCollection matches = regex.Matches(input);
+            
+            foreach (Match match in matches)
+            {
+                totalPoints += match.Groups["destination"].Value.Length;
+                destinations.Add(match.Groups["destination"].Value);
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {totalPoints}");
         }
     }
 }
