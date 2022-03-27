0633d015f0f84a13c7b8f6eedd04feb6df0e76c7
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-03-27 17:45:16 +0200

    Upload

---
+++ 03. Programming Fundamentals Final Exam Retake/01. Secret Chat/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _01._Secret_Chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "Reveal")
+            {
+                string[] commandArg = command.Split(":|:", StringSplitOptions.RemoveEmptyEntries);
+                string commandType = commandArg[0];
+
+                if (commandType == "InsertSpace")
+                {
+                    int index = int.Parse(commandArg[1]);
+
+                    message = message.Insert(index, " ");
+
+                    Console.WriteLine(message);
+                }
+                else if (commandType == "Reverse")
+                {
+                    string subString = commandArg[1];
+
+                    if (message.Contains(subString))
+                    {
+                        int startIdx = message.IndexOf(subString);
+                        int endIdx = startIdx + subString.Length;
+                        int count = endIdx - startIdx;
+
+
+                        char[] charArray = subString.ToCharArray();
+                        Array.Reverse(charArray);
+                        string reversedString = new string(charArray);
+
+                        message = message.Remove(startIdx, count);
+                        message = message + reversedString;
+
+                        Console.WriteLine(message);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                }
+                else if (commandType == "ChangeAll")
+                {
+                    string stringToReplace = commandArg[1];
+                    string replacment = commandArg[2];
+
+                    message = message.Replace(stringToReplace, replacment);
+
+                    Console.WriteLine(message);
+                }
+            }
+
+            Console.WriteLine($"You have a new text message: {message}");
+        }
+    }
+}
---
+++ 03. Programming Fundamentals Final Exam Retake/02. Mirror Words/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Mirror_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"(\@|\#)(?<first>[A-za-z]{3,})(\1){2}(?<second>[A-za-z]{3,})(\1)");
+            MatchCollection matches = regex.Matches(input);
+
+            List<string> pairs = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                string firstWord = match.Groups["first"].Value;
+                string secondWord = match.Groups["second"].Value;
+
+                char[] charArray = secondWord.ToCharArray();
+                Array.Reverse(charArray);
+                string reversedSecondWord = new string(charArray);
+
+                if (firstWord == reversedSecondWord)
+                {
+                    pairs.Add($"{firstWord} <=> {secondWord}");
+                }
+            }
+
+            Console.WriteLine(matches.Count > 0 ? $"{matches.Count} word pairs found!" : "No word pairs found!");
+
+            if (pairs.Count > 0)
+            {
+                Console.WriteLine("The mirror words are:");
+                Console.WriteLine(String.Join(", ", pairs));
+            }
+            else
+            {
+                Console.WriteLine("No mirror words!");
+            }
+        }
+    }
+}
