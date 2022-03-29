a91d032095ad640b79117cdfd4ec12bbffc417b2
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-03-29 15:18:05 +0300

    Update

---
+++ 04. Programming Fundamentals Final Exam/02. Fancy Barcodes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                Regex regex = new Regex(@"(\@\#+)([A-Z][A-Za-z0-9]{4,}[A-Z])(\@\#+)");
+                Match match = regex.Match(input);
+                
+                if (match.Success)
+                {
+                    char[] digits = input.Where(x => char.IsDigit(x)).ToArray();
+
+                    if (digits.Length == 0)
+                    {
+                        Console.WriteLine($"Product group: 00");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Product group: {string.Join("", digits)}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+
+            }
+        }
+    }
+}
