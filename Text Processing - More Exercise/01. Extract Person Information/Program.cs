aea67a37a864e7eadd42cb4ca380376f36deac86
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-03-30 15:25:03 +0300

    update

---
+++ Text Processing - More Exercise/01. Extract Person Information/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _01._Extract_Person_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+
+                int idxBeforeName = input.IndexOf('@');
+                int idxAfterOfName = input.IndexOf('|');
+
+                string name = input.Substring(idxBeforeName + 1, (idxAfterOfName - idxBeforeName) - 1);
+
+                int idxBeforeAge = input.IndexOf('#');
+                int idxAfterAge = input.IndexOf('*');
+
+                string age = input.Substring(idxBeforeAge + 1, (idxAfterAge - idxBeforeAge) - 1);
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+
+        }
+    }
+}
