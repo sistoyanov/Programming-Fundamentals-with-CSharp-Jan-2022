5bfb41510a0c93f5fd97dc9acd2a4d897eb5a625
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-03-31 10:59:11 +0300

    Upload

---
+++ Objects and Classes - More Exercise/02. Oldest Family Member/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Oldest_Family_Member
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            List<Person> Family = new List<Person>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] personDetails = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personDetails[0];
+                int age = int.Parse(personDetails[1]);
+
+                Family.Add(new Person(name, age));
+            }
+
+            Person oldestPerson = Family.OrderByDescending(a => a.Age).First();
+            Console.WriteLine($"{oldestPerson.Name} {oldestPerson.Age}");
+            
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
