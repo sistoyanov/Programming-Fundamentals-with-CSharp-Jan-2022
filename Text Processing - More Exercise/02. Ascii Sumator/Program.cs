c185ec7dc3da275994bac780d7fb356e2ef0bb63
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-04-01 09:59:20 +0300

    update

---
+++ Text Processing - More Exercise/02. Ascii Sumator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02._Ascii_Sumator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            char startChar = char.Parse(Console.ReadLine());
+            char endChar = char.Parse(Console.ReadLine());
+            char[] input = Console.ReadLine().ToCharArray();
+
+            foreach (char item in input)
+            {
+                if (item > startChar && item < endChar)
+                {
+                    sum += item;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
