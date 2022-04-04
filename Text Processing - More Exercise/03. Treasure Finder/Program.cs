63fc7510618038f3f0853e0779b7b12d091dfeae
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-04-04 11:36:18 +0300

    Upload

---
+++ Text Processing - More Exercise/03. Treasure Finder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Treasure_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
