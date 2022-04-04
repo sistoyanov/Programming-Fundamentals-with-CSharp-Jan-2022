915f414fcf6f75e861c580c672a503f5c3f3d3dd
Stoyan Stoyanov <si_stoyanov@outlook.com>
2022-04-04 17:02:57 +0300

    Upload

--- Text Processing - More Exercise/03. Treasure Finder/Program.cs
+++ Text Processing - More Exercise/03. Treasure Finder/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 
 namespace _03._Treasure_Finder
 {
@@ -6,7 +8,34 @@ namespace _03._Treasure_Finder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] key = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            string input = string.Empty;
+            
+
+            while ((input = Console.ReadLine()) != "find")
+            {
+                string message = input;
+                StringBuilder output = new StringBuilder();
+                int keyIdx = -1;
+                int counter = 1;
+
+                for (int i = 0; i < message.Length; i++)
+                {
+                    
+                    if (i >= counter * key.Length)
+                    {
+                        keyIdx = i - (counter * key.Length);
+                        counter++;
+                    }
+                    else
+                    {
+                        keyIdx ++;
+                    }
+
+                    output.Append((char)(message[i] - key[keyIdx]));
+
+                }
+            }
         }
     }
 }
